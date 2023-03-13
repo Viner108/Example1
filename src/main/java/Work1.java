8ac5faa83e2aa3c63392ff8b14d801b6b2950a6e
Viner108 <andrukovichl@mail.ru>
2023-03-13 16:37:08 +0300

    test commit 7

--- src/main/java/Work1.java
+++ src/main/java/Work1.java
@@ -6,6 +6,7 @@ public class Work1 {
     public static void main(String[] args) {
         int counter = 0;
         int counter2 = 0;
+        int counter3 = 0;
         long size = 0;
         int k = 0;
         byte[] bytes = null;
