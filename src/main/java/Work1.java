704ad92ee3529bc84fbd43d3530c75ca7e7dac4f
Viner108 <andrukovichl@mail.ru>
2023-03-13 16:23:00 +0300

    counter renamed

--- src/main/java/Work1.java
+++ src/main/java/Work1.java
@@ -4,7 +4,7 @@ import java.nio.file.Path;
 
 public class Work1 {
     public static void main(String[] args) {
-        int j = 0;
+        int counter = 0;
         int k = 0;
         byte[] bytes = null;
         try {
